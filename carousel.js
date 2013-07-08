6219d8297b387c32219544d458855a069ca08385
Stephane Thomas <thomas.stephn@gmail.com>
2013-07-08 09:36:10 -0700

    Turn into object, add another carousel modified:   carousel.js modified:   main.html modified:   styles.css

--- carousel.js
+++ carousel.js
@@ -1,44 +1,119 @@
-var photos = [];
-var position = 0;
+function Carousel(container){
+  this.container = container;
+  this.photos = [];
+  this.position = 0;
+}
 
-function flickr(word) {
+
+Carousel.prototype.processPhotos = function(data){
+  var self = this;
+  self.position = 0;
+  self.photos = [];
+  console.log("photos of " + self.container);
+  $.each(data.photos.photo, function(index, photo){
+    self.photos.push(urlPhoto(photo));
+  });
+  self.appendPhoto(self.photos[self.position]);
+};
+
+Carousel.prototype.flickr = function(word) {
+  var self = this;
  $.getJSON('http://api.flickr.com/services/rest/?jsoncallback=?', {
   method: 'flickr.photos.search',
   api_key: 'b87176d912b9ca01fbabb271de79e769',
   text: word,
   content_type: 1, //return photo only
   format: 'json'
-}).success(function(data) {
-  processPhotos(data);
-}).fail(function(error){
-  console.log("Warning" + error);
-});
-}
-
-function processPhotos(data){
-  position = 0;
-  photos = [];
-  $.each(data.photos.photo, function(index, photo){
-    photos.push(urlPhoto(photo));
+  }).success(function(data) {
+    self.processPhotos(data);
+  }).fail(function(error){
+    console.log("Warning" + error);
   });
-  appendPhoto(photos[position]);
-}
-function urlPhoto(photo) {
-  return "http://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg";
-}
+};
 
-function appendPhoto(photo) {
-  $('#photo').html("<img src=" + photo +">");
-  preload();
-}
+Carousel.prototype.appendPhoto = function(photo) {
+  var self = this;
+  $(self.container).find(".photo").html("<img src=" + photo +">");
+  self.preload();
+};
 
-function preload() {
-  $('#preload').html("");
+Carousel.prototype.preload = function() {
+  var self = this;
+  var div = $(self.container).find('.preload');
+  div.html("");
   for (i = 1; i < 7; i++) {
-    $('#preload').append("<img src=" + photos[nTimes(nextPosition, position, i)] +">");
-    $('#preload').append("<img src=" + photos[nTimes(pastPosition, position, i)] +">");
+    div.append("<img src=" + self.photos[nTimes(nextPosition, [self.photos, self.position], i)[1]] +">");
+    div.append("<img src=" + self.photos[nTimes(pastPosition, [self.photos, self.position], i)[1]] +">");
   }
-}
+};
+
+Carousel.prototype.searchWord = function(e, own) {
+  var self = this;
+  e.preventDefault();
+  var word = $(own).find('input[name="word"]').val();
+  self.flickr(word);
+};
+
+Carousel.prototype.moveBack = function() {
+  var self = this;
+  self.position = pastPosition([self.photos, self.position])[1];
+  self.appendPhoto(self.photos[self.position]);
+};
+
+Carousel.prototype.moveForward = function() {
+  var self = this;
+  self.position = nextPosition([self.photos, self.position])[1];
+  self.appendPhoto(self.photos[self.position]);
+};
+
+Carousel.prototype.keyMove = function(e) {
+  var self = this;
+  if (e.keyCode == 37) {
+    self.moveBack();
+  } else if (e.keyCode == 39) {
+    self.moveForward();
+  }
+};
+
+Carousel.prototype.listeners = function(e) {
+  var self = this;
+  $(self.container).keydown(function(e){
+    self.keyMove(e);
+  });
+
+  $(self.container).on('submit', 'form', function(e){
+    self.searchWord(e, this);
+  });
+
+  $(self.container).on("click", 'button', function(e){
+    e.preventDefault();
+    if ($(this).attr("name") === "left") {
+      self.moveBack();
+    } else if ($(this).attr("name") === "right") {
+      self.moveForward();
+    }
+  });
+};
+
+function pastPosition(args) {
+  var photos = args[0];
+  var current = args[1];
+  if (current === 0) {
+    return [photos,(photos.length - 1)];
+  } else {
+    return [photos,(current - 1)];
+  }
+};
+
+function nextPosition(args) {
+  var photos = args[0];
+  var current = args[1];
+  if (current === (photos.length - 1)) {
+    return [photos,0];
+  } else {
+    return [photos,(current + 1)];
+  }
+};
 
 function nTimes(fn, element, n, i) {
   i = i || 0;
@@ -50,62 +125,20 @@ function nTimes(fn, element, n, i) {
   }
 }
 
-function pastPosition(current){
-  if (current === 0) {
-    return (photos.length - 1);
-  } else {
-    return (current - 1);
-  }
-}
-
-function nextPosition(current) {
-  if (current === (photos.length - 1)) {
-    return 0;
-  } else {
-    return (current + 1);
-  }
-}
-
-function searchWord(e, self) {
-  e.preventDefault();
-  var word = $(self).find('input[name="word"]').val();
-  flickr(word);
-}
-
-function keyMove(e) {
-  if (e.keyCode == 37) {
-    moveBack();
-  } else if (e.keyCode == 39) {
-    moveForward();
-  }
-}
-
-function moveBack() {
-  position = pastPosition(position);
-  appendPhoto(photos[position]);
-}
-
-function moveForward() {
-  position = nextPosition(position);
-  appendPhoto(photos[position]);
+function urlPhoto(photo) {
+  return "http://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg";
 }
 
 
 $(document).on('ready', function(){
-  $('form').on('submit', function(e){
-    searchWord(e, this);
+  var carouselFirst = new Carousel("#carousel-1");
+  var carouselSecond = new Carousel("#carousel-2");
+
+  $(carouselFirst.container).on('click', function(e) {
+    carouselFirst.listeners(e);
   });
 
-  $(document).keydown(function(e){
-    keyMove(e);
-  });
-
-  $('button').on("click", function(e){
-    e.preventDefault();
-    if ($(this).attr("name") === "left") {
-      moveBack();
-    } else if ($(this).attr("name") === "right") {
-      moveForward();
-    }
+  $(carouselSecond.container).on('click', function(e) {
+    carouselSecond.listeners(e);
   });
 });
